a44d86ff7ded5ae9bb5a4a135cadc0b3de5e43f0
Changhoon Han <shirohoo4102@gmail.com>
2025-01-18 22:11:34 +0900

    Add ProfileResponseTest

---
+++ api/src/test/java/io/zhc1/realworld/api/response/ProfileResponseTest.java
@@ -0,0 +1,27 @@
+package io.zhc1.realworld.api.response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import io.zhc1.realworld.core.model.User;
+
+class ProfileResponseTest {
+
+    @Test
+    void shouldReturn_Username_From_UserProfile() {
+        // given
+        String email = "a@example.com";
+        String username = "LeoLeeTech";
+        String password = "password";
+
+        User user = new User(email, username, password);
+
+        // when
+        ProfileResponse sut = ProfileResponse.from(user);
+
+        // then
+        assertThat(sut.username()).isNotEqualTo(email);
+        assertThat(sut.username()).isEqualTo(username);
+    }
+}
