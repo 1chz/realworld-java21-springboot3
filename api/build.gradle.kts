24187b9cb153ae08e22a7f84cb05e033fa639c1b
Changhoon Han <shirohoo4102@gmail.com>
2025-01-18 22:11:40 +0900

    Remove Jakarta Persistence API related warnings

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,4 +3,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+    // Remove Jakarta Persistence API related warnings
+    implementation("jakarta.persistence:jakarta.persistence-api")
 }
