870f8d1b440dad8e9803313b765e2340e26b3273
Changhoon Han <shirohoo4102@gmail.com>
2025-03-23 05:20:02 +0900

    chore: update Gradle properties for improved build performance
    - Enabled Gradle daemon
    - Re-enabled parallel execution and configure on demand
    
    (cherry picked from commit e218e9c629835d90afe1ea8671f744a58090419b)

--- module/core/build.gradle.kts
+++ module/core/build.gradle.kts
@@ -5,4 +5,7 @@ plugins {
 dependencies {
     implementation("jakarta.persistence:jakarta.persistence-api")
     testFixturesImplementation("jakarta.persistence:jakarta.persistence-api")
+
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("com.github.ben-manes.caffeine:caffeine")
 }
