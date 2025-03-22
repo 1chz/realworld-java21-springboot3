2b714c0d263330704c57c879552f8f0d5c645f28
Changhoon Han <shirohoo4102@gmail.com>
2025-03-23 05:19:52 +0900

    feat: integrate caching mechanism
    - Added Spring Boot Starter Cache dependency to Core's `build.gradle.kts`.
    - Implemented caching in `TagService` with `@Cacheable` annotation.
    - Created `CacheConfiguration` class for cache settings.
    
    (cherry picked from commit fd21423ab94026fc37d30fa303aaf3664191c0ac)

---
+++ module/core/src/main/java/io/zhc1/realworld/core/constant/Cache.java
@@ -0,0 +1,9 @@
+package io.zhc1.realworld.core.constant;
+
+public final class Cache {
+    private Cache() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated!");
+    }
+
+    public static final String ALL_TAGS = "all-tags";
+}
--- module/core/src/main/java/io/zhc1/realworld/core/service/TagService.java
+++ module/core/src/main/java/io/zhc1/realworld/core/service/TagService.java
@@ -2,10 +2,12 @@ package io.zhc1.realworld.core.service;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+import io.zhc1.realworld.core.constant.Cache;
 import io.zhc1.realworld.core.model.Tag;
 import io.zhc1.realworld.core.model.TagRepository;
 
@@ -19,6 +21,7 @@ public class TagService {
      *
      * @return Returns all tags
      */
+    @Cacheable(value = Cache.ALL_TAGS)
     public List<Tag> getAllTags() {
         // Note: If there are too many tags, recommend apply cursor based pagination.
         return tagRepository.findAll();
